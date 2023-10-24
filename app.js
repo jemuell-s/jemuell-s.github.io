27535d5a5649ab5d4c85edc39e4b43c53891c1ae
jemuell-s <jemuell.smith@sjsu.edu>
2023-10-24 10:05:37 -0700

    contact page finished

--- app.js
+++ app.js
@@ -7,4 +7,7 @@
             document.getElementById(button.dataset.id).classList.add('active');
         })
     });
+    document.querySelector(".theme-button").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
 })();
